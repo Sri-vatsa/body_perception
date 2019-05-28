5ac63816d8a337ba13e9adae549516b18504db61
Srivatsa <p.psrivatsa@gmail.com>
2019-05-28 21:09:33 +0200

    Run inferece on image loaded & post processing (work in progress)

--- index_dev.js
+++ index_dev.js
@@ -17,23 +17,67 @@ const img = new Image();
 var estimate = async function (imageElement, outputStride, segmentationThreshold) {
     const net = await bodyPix.load(1.0);
     let segmentation = await net.estimatePersonSegmentation(imageElement, outputStride, segmentationThreshold);
-    return await segmentation;
+
+    return segmentation;
 };
 
-img.onload = () => {
+img.onload = async () => {
     console.log("image loaded.");
-    ctx.fillStyle ="#000";
+    ctx.fillStyle = "#000";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(img, 0, 0, width, height);
-    //const imageElement = tf.browser.fromPixels(img);
-    console.log(canvas);
+
     console.log(outputStride);
     console.log(segmentationThreshold);
-    countPixels()
-    /*estimate(canvas, outputStride, segmentationThreshold).then(function (segmentation) {
-        console.log("Test");
-        console.log("Segmentation: " + function () { return JSON.stringify(segmentation, null, ' ') });
-    });*/
+    //countPixels()
+
+    // Estimate body segment
+    var segmentation = await estimate(canvas, outputStride, segmentationThreshold);
+    console.log(segmentation)
+
+    /*
+    // Plot segmentation mask
+    var palette = ctx.getImageData(0,0,canvas.width,canvas.height);
+    palette.data.set(new Uint8ClampedArray(segmentation.data));
+    ctx.putImageData(palette,0,0);
+    
+    // Write image to file
+    var image = new Image();
+
+    const fs = require('fs');
+    image.src = canvas.toDataURL("image/png");
+    image.onload = () => {
+        fs.writeFile("test.png", image.src, function(err) {
+            if(err) {
+                return console.log(err);
+            }
+        
+            console.log("The file was saved!");
+        }); 
+    }
+    */
+    //Post processing * DOESNT WORK *
+    const maskBackground = true;
+    const backgroundDarkeningMask = bodyPix.toMaskImageData(segmentation, maskBackground);
+
+    const opacity = 0.9;
+    const maskBlurAmount = 0;
+    const flipHorizontal = true;
+
+    bodyPix.drawMask(canvas, img, backgroundDarkeningMask, opacity, maskBlurAmount, flipHorizontal);
+    
+    /*
+    var fs = require('fs');
+
+    var data = {}
+    data.table = []
+    data.table.push(segmentation)
+    fs.writeFile("input.json", JSON.stringify(data), function (err) {
+        if (err) throw err;
+        console.log('complete');
+    }
+    );
+    */
 }
 
 img.onerror = err => { throw err }
@@ -51,5 +95,5 @@ function countPixels() {
             }
         }
     }
-console.log(nAlive);
+    console.log(nAlive);
 }
